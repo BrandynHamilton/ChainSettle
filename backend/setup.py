36c9dd7402b8593902c4392e13ac26227f06dd3f
BrandynHamilton <brandynham1120@gmail.com>
2025-04-22 14:52:18 -0500

    additional setup

---
+++ backend/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="chainsettle",  
+    version="0.1.0", 
+    packages=find_packages(),
+    install_requires=[
+        "pandas",
+        "numpy",
+        "dotenv",
+        "flask",
+        "web3"
+    ],
+    author="Brandyn Hamilton",
+    author_email="brandynham1120@gmail.com",
+    description="Python Library for ChainSettle Nodes.",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url=" ",  
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",  
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.7',
+)
